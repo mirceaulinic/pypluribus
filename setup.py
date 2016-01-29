66ff0b40c3ecc663ba2e396c0d9295401a88ad2b
Mircea Ulinic <mirucha@cloudflare.com>
2016-01-29 11:29:11 +0000

    New release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 
 reqs = [str(ir.req) for ir in install_reqs]
 
-version = '0.0.1'
+version = '0.0.2'
 
 setup(
     name                 = 'pyPluribus',
